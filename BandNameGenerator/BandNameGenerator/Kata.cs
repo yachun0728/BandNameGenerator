cf8d71bed9410ba735296506a1eaca28faddcbf4
yachun0728 <yachun0728@gmail.com>
2017-08-11 15:38:45 +0800

    Production code

---
+++ BandNameGenerator/BandNameGenerator/Kata.cs
@@ -0,0 +1,24 @@
+﻿namespace BandNameGenerator
+{
+    public class Kata
+    {
+        public static string BandNameGenerator(string str)
+        {
+            var firstCharacter = str.Substring(0,1);
+            var lastCharacter = str.Substring(str.Length-1);
+            var upperStr = GetFirstLetterToUpper(str);
+
+            if (firstCharacter == lastCharacter)
+            {
+                return upperStr + upperStr.Substring(1, str.Length-1);
+            }
+
+            return ("The " + upperStr);
+        }
+
+        private static string GetFirstLetterToUpper(string str)
+        {
+            return str.Substring(0, 1).ToUpper() + str.Substring(1);
+        }
+    }
+}
---
+++ BandNameGenerator/BandNameGeneratorTests/KataTests.cs
@@ -0,0 +1,33 @@
+﻿using BandNameGenerator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BandNameGeneratorTests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void Input_knife_will_get_The_Knife()
+        {
+            Assert.AreEqual("The Knife", Kata.BandNameGenerator("knife"));
+        }
+
+        [TestMethod()]
+        public void Input_tart_will_get_Tartart()
+        {
+            Assert.AreEqual("Tartart", Kata.BandNameGenerator("tart"));
+        }
+
+        [TestMethod()]
+        public void Input_sandles_will_get_Sandlesandles()
+        {
+            Assert.AreEqual("Sandlesandles", Kata.BandNameGenerator("sandles"));
+        }
+
+        [TestMethod()]
+        public void Input_bed_will_get_The_Bed()
+        {
+            Assert.AreEqual("The Bed", Kata.BandNameGenerator("bed"));
+        }
+    }
+}
